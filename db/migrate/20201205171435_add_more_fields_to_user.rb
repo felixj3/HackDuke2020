c5d77211fb28fead102d6501b1b2fd08cfcb84e3
fj32 <fj32@duke.edu>
2020-12-05 11:54:05 -0600

    delete old users

--- db/migrate/20201205171435_add_more_fields_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddMoreFieldsToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :email, :string
-    add_column :users, :clubs, :string # can't have a list of clubs
-  end
-end
