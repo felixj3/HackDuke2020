d39722b2e8de3afbecf4b0f815af18b36e97790e
fj32 <fj32@duke.edu>
2020-12-05 16:01:58 -0600

    change belongs to and has many syntax

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_05_204646) do
+ActiveRecord::Schema.define(version: 2020_12_05_214750) do
+
+  create_table "study_rooms", force: :cascade do |t|
+    t.integer "rank"
+    t.integer "number_student"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tutors", force: :cascade do |t|
     t.string "subject"
