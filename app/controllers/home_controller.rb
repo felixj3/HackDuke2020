7f30ff1b5c8cf0c58458431b35cc3d216b694fee
fj32 <fj32@duke.edu>
2020-12-05 17:25:56 -0600

    clubs appear in dropdown

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,22 +6,7 @@ class HomeController < ApplicationController
     # occurs when someone presses profile button on nav bar
     puts("----------------------------------------\n")
     puts("pre")
-    temp = File.open("#{Rails.root}/mlModel/Parsers/clubs.json").read # relative to current location of file
-    render :json => temp
-    data = JSON.parse(temp)
-    @clubs = Array.new
-    data.each do |k,v|
-      puts "key: #{k} value: #{v}"
-      v.each do |c|
-        @clubs << c
-      end
-    end
-    
-    puts("----------------------------------------\n")
-    puts("felix")
-    @clubs.each do |c|
-      puts c
-    end
+    getClubs
   end
 
   def post
@@ -31,5 +16,28 @@ class HomeController < ApplicationController
         puts "key: #{k} value: #{v}"
     end
     puts(params[:name])
+  end
+
+  def getClubs
+    temp = File.open("#{Rails.root}/mlModel/Parsers/clubs.json").read # relative to current location of file
+    # render :json => temp
+    data = JSON.parse(temp)
+    @categoryAndClubs = data
+    @clubs = Array.new
+    data.each do |k,v|
+      # puts "key: #{k} value: #{v}"
+      v.each do |c|
+        puts c
+        @clubs << c
+      end
+    end
+    
+    puts("----------------------------------------\n")
+    puts("inside getClubs\n")
+    @clubs.each do |c|
+      puts c
+    end
+    @clubs.sort!
+    @clubs.uniq!
   end
 end
