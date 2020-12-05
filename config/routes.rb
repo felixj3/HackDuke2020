f80b6e87803bea094ab2804da5a9a0532add4b0c
fj32 <fj32@duke.edu>
2020-12-05 12:44:42 -0600

    routes for assist

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "home#index"
   devise_for :users
   get "request" => "request_tutor#index"
+  get "assist" => "become_tutor#index"
   # get "users/sign_in", to: "static_pages#mission", as: "mission"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
