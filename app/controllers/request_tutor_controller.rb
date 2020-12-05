00ee97adb3d947231ee80af006aefe482c4b29d4
fj32 <fj32@duke.edu>
2020-12-05 13:45:09 -0600

    get data from form

--- app/controllers/request_tutor_controller.rb
+++ app/controllers/request_tutor_controller.rb
@@ -2,5 +2,9 @@ class RequestTutorController < ApplicationController
     def post
         puts("----------------------------------------\n")
         puts("hi")
+        params.each do |k,v|
+            puts "key: #{k} value: #{v}"
+        end
+        puts(params[:name])
     end
 end
