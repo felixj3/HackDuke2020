38f3386220b4a947e53b3463a1b5090aa8f225ff
fj32 <fj32@duke.edu>
2020-12-05 12:37:23 -0600

    routes work now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "home#index"
+  devise_for :users
   get "request" => "request_tutor#index"
   # get "users/sign_in", to: "static_pages#mission", as: "mission"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
