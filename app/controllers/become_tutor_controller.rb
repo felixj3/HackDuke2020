c10f23e9fe5ec6eeb0f6401db95079e566eecf2c
fj32 <fj32@duke.edu>
2020-12-05 22:35:31 -0600

    courses are sorted

--- app/controllers/become_tutor_controller.rb
+++ app/controllers/become_tutor_controller.rb
@@ -9,16 +9,20 @@ class BecomeTutorController < ApplicationController
     def getCourses
         puts("inside getCourses\n")
         temp = File.open("#{Rails.root}/mlModel/Courses/output.json").read # relative to current location of file
-        render :json => temp
+        # render :json => temp
         data = JSON.parse(temp)
-        @courseAbbreviationsAndNames = data
+        @courseAbbreviationsAndNames = Hash.new
+        data.sort.map do |k, v|
+            @courseAbbreviationsAndNames[k] = v
+        end
+
 
         @courseAbbreviationsAndNames.each do |k, v|
-            puts "key: #{k}"
-            puts("----------------------------------------\n")
-            v.each do |array|
-                puts "#{array[0]}, #{array[1]}"
-            end
+            # puts "key: #{k}"
+            # puts("----------------------------------------\n")
+            # v.each do |array|
+            #     puts "#{array[0]}, #{array[1]}"
+            # end
         end
         
         puts("----------------------------------------\n")
