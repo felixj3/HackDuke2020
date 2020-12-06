b1c8b594651f739851b483d24ab04483d93a5fdb
fj32 <fj32@duke.edu>
2020-12-06 14:02:43 -0600

    seed more data

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,20 @@ User.create(email: "hi@gmail.com")
 User.create!(email: "alsdddj@gmail.com", password: "111111", category1: 0, category2: 0, category3: 0, category4: 0, category5: 5, category6: 0, category7: 0, category8: 0, category9: 0, category10: 0, category11: 0, category12: 0, name: "George", gradYear: 2023, subject1: "Mathmatics (MATH)", course1: "221", subject2: "Mathmatics (MATH)", course2: "212", subject3: "", course3: "", subject4: "", course4: "", subject5: "", course5: "", hometown: "Durham", club1: "Advocates for MD/PhD Women in Science", club2: "Alpha Delta Phi", club3: "eNable - Duke University Chapter", club4: "eNable - Duke University Chapter", club5: "eNable - Duke University Chapter", major: "Arabic (ARABIC)", study_room_id: nil)
 User.create!(email: "alsdj@gmail.com", password: "111111", category1: 0, category2: 0, category3: 0, category4: 0, category5: 5, category6: 0, category7: 0, category8: 0, category9: 0, category10: 0, category11: 0, category12: 0, name: "Felix", gradYear: 2023, subject1: "Mathmatics (MATH)", course1: "218", subject2: "Mathmatics (MATH)", course2: "340", subject3: "", course3: "", subject4: "", course4: "", subject5: "", course5: "", hometown: "Charlotte", club1: "Advocates for MD/PhD Women in Science", club2: "Alpha Delta Phi", club3: "eNable - Duke University Chapter", club4: "eNable - Duke University Chapter", club5: "eNable - Duke University Chapter", major: "Arabic (ARABIC)", study_room_id: nil)
 
-Tutor.create(subject: "MATH", number: "221", grade: "A", availability: "3", pay: "15")
-Tutor.create(subject: "CS", number: "221", grade: "A", availability: "3", pay: "15")
-Tutor.create(subject: "PHILO", number: "221", grade: "A", availability: "3", pay: "15")
+a = Tutor.create(subject: "MATH", number: "221", grade: "A", availability: "3", pay: "15")
+b = Tutor.create(subject: "CS", number: "221", grade: "A", availability: "3", pay: "15")
+c = Tutor.create(subject: "PHILO", number: "221", grade: "A", availability: "3", pay: "15")
+User.first.tutors << a
+User.first.tutors << b
+User.first.tutors << c
+User.first.save
+
+a =Tutor.create(subject: "CHEM", number: "201", grade: "A", availability: "3", pay: "15")
+b = Tutor.create(subject: "CHINESE", number: "201", grade: "A", availability: "3", pay: "15")
+c = Tutor.create(subject: "THEATERST", number: "145", grade: "A", availability: "3", pay: "15")
+User.second.tutors << a
+User.second.tutors << b
+User.second.tutors << c
+User.second.save
+
 # StudyRoom.create(rank: 5, number_student: 10)
