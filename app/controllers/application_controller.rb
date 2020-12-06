9bfd42d732815f5ee59a56935ae56d1e91fc0e8d
Rui Xin <rui.xin926@duke.edu>
2020-12-05 23:36:20 -0500

    update python connection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
+require 'net/http'
+require 'json'
+
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :null_session
     # deactivative CSRF token authenticity
+
+    def contact_python(dict = {})
+        uri = URI('http://localhost:' + $BACKBACKENDPORT.to_s)
+        res = Net::HTTP.post_form(uri, 'data' => dict.to_json)
+        return JSON.parse(res.body)
+    end
 end
