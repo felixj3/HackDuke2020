6d98065867980f4bc6ed5a576ddba35cf2223122
fj32 <fj32@duke.edu>
2020-12-06 01:46:46 -0600

    just need to save values into database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -62,7 +62,12 @@ class HomeController < ApplicationController
     user.update(category1: categories[0], category2: categories[1], category3: categories[2], category4: categories[3], category5: categories[4], category6: categories[5], category7: categories[6], category8: categories[7], category9: categories[8], category10: categories[9], category11: categories[10], category12: categories[11], name: params[:name])
 
     getCourses
-    @courseAbbreviationsAndNames
+    # @courseAbbreviationsAndNames
+    params.each do |k, v|
+      puts "key: #{k}, value: #{v}"
+    end
+
+    # user.update(subject1: , course1: )
 
     redirect_to root_url
   end
